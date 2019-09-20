1f9b7d607ae3860c0aaa92bf71fbdc1b8c78d646
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2019-09-21 00:01:08 +0100

    inital setup

---
+++ src/OcelotApiGateways/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotApiGateways
+{
+    //https://docs.microsoft.com/pl-pl/dotnet/architecture/microservices/multi-container-microservice-net-applications/implement-api-gateways-with-ocelot
+    //https://altkomsoftware.pl/en/blog/building-api-gateways-with-ocelot/
+    //https://ocelot.readthedocs.io/en/latest/index.html
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(Path.Combine("configuration", $"hostsettings.json"), optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            //CreateWebHostBuilder(args).Build().Run();
+            var builder = CreateWebHostBuilder(args);
+            var host = builder.Build();
+
+            foreach (var address in host.ServerFeatures
+                .Get<IServerAddressesFeature>()
+                .Addresses)
+            {
+                Console.WriteLine($"listening on {address}");
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging((hostingContext, logging)=> 
+            {
+                var env = hostingContext.HostingEnvironment;
+                Console.WriteLine(env.EnvironmentName);
+                if (env.IsDevelopment())
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                }
+                else
+                {
+                    logging.AddEventSourceLogger();
+                }
+            })
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile(Path.Combine("configuration", $"hostsettings.json"), optional: true, reloadOnChange: true)
+                .AddJsonFile(Path.Combine("configuration", $"configuration.json"), optional: true, reloadOnChange: true)
+                .AddJsonFile(Path.Combine("configuration", $"configuration.{env.EnvironmentName}.json"), optional: true, reloadOnChange: true);
+
+                config.AddEnvironmentVariables();
+            })
+            .UseStartup<Startup>();
+    }
+}
---
+++ src/OcelotApiGateways/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Eureka;
+using Ocelot.Cache.CacheManager;
+
+namespace OcelotApiGateways
+{
+    public class Startup
+    {
+        private readonly IConfiguration _cfg;
+
+        public Startup(IConfiguration configuration) => _cfg = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot()
+                //.AddEureka()
+                .AddCacheManager(x => x.WithDictionaryHandle()); ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOcelot();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
